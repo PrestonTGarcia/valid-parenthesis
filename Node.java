ef01bdcab0f152261860f07922ab8853e730cbb4
Preston Garcia <PrestonTGarcia@gmail.com>
2021-03-07 23:18:16 -1000

    Finished Node Definition.

---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node{ // Node class
+
+   private char data;
+   private Node prev;
+   private Node next;
+   
+   public Node(char data){ // Node constructor
+   
+      this.data = data;
+   
+   } // Node constructor
+
+} // Node class
