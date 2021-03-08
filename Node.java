6a7d95e944a6a1edf5ff598a6f85de8dbe48fc6c
Preston Garcia <PrestonTGarcia@gmail.com>
2021-03-07 23:42:52 -1000

    Added Node Methods.

--- Node.java
+++ Node.java
@@ -1,13 +1,37 @@
 public class Node{ // Node class
 
    private char data;
-   private Node prev;
-   private Node next;
+   private Node prev = null;
+   private Node next = null;
    
    public Node(char data){ // Node constructor
    
       this.data = data;
    
    } // Node constructor
+   
+   public void setNext(Node newNode){ // setNext method
+   
+      this.next = newNode;
+   
+   } // setNext method
+   
+   public void setPrev(Node newNode){ // setPrev method
+   
+      this.prev = newNode;
+   
+   } // setPrev method
+   
+   public Node getNext(){ // getNext method
+   
+      return this.next;
+   
+   } // getNext method
+   
+   public Node getPrev(){ // getPrev method
+   
+      return this.prev;
+   
+   } // getPrev method
 
 } // Node class
